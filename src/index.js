7538b734d2edf3b6079d94bc42b7079eef331ac9
Sergio Tovar <stovarm@gmail.com>
2021-08-10 18:07:06 -0500

    Upgrade the visual writting nothing change

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ function calculateCinema (movies, cost) {
     return resultCinema;
 };
 
-
-
 function calculateStreaming() {
     const checks = document.getElementsByClassName('check');   //Toma todos los checkbox
     let totalStreaming = 0;
@@ -23,18 +21,23 @@ function compareStreaming() {     //Esta funcion es la que activa el Botón
     const resultCinema = calculateCinema (movies, ticketPrice); 
     const resultStreaming = calculateStreaming();
     const resultCompare = resultCinema - resultStreaming;
+
     if(resultCompare > 0){  //Si el resultado es Positivo quiere decir que El total que se paga por ir a Cine es Mayor al de pagar las plataformas seleccionadas.
         const resultPlatform = document.getElementById("ResultPlatform");  //Enlazar la const con el elemento p donde voy a dar el resultado
         resultPlatform.innerText = " $" + resultStreaming; //Escribir en HTML desde JS
+
         const resultMovies = document.getElementById("ResultCinema");
         resultMovies.innerText = " $" + resultCinema;
+
         const Compare = document.getElementById("ResultCompare")
         Compare.innerText = "It's better pay the Platforms";
     } else {
         const resultPlatform = document.getElementById("ResultPlatform");
         resultPlatform.innerText = " $" + resultStreaming;
+
         const resultMovies = document.getElementById("ResultCinema");
         resultMovies.innerText = " $" + resultCinema;
+        
         const Compare = document.getElementById("ResultCompare")
         Compare.innerText = "It's better go to the Cinema";        
     };
